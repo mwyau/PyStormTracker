34c977cc53376046976ea23c85f6dfc19b006cb5
Albert Yau <mwyau@users.noreply.github.com>
2015-07-22 13:28:35 -0600

    Update detector.py
    - Define Center.__str__() to print time, lat, lon and the variable
    - Rewrite RectGrid._init() to use automatic lazy initialization
    - Add bound check for get_var() and get_time()
    - RectGrid.detect() returns a list of list of Center’s, while each
    sublist is Center’s from each time step (chart)

--- pystormtracker/detector.py
+++ pystormtracker/detector.py
@@ -44,10 +44,12 @@ class Center(object):
         self.lon = lon
         self.var = var
 
-    def __str__(self):
+    def __repr__(self):
         return str(self.var)
 
-    __repr__ = __str__
+    def __str__(self):
+        return "[time="+str(self.time)+", lat="+str(self.lat)+", lon="+ \
+                str(self.lon)+ ", var="+str(self.var)+"]"
 
     @classmethod
     def abs_dist(cls, center1, center2):
@@ -77,60 +79,127 @@ class Center(object):
 
 class RectGrid(Grid):
 
-    def __init__(self, pathname, varname, trange=None, open_file=True):
+    def __init__(self, pathname, varname, trange=None):
 
         self.pathname = pathname
         self.varname = varname
         self.trange = trange
 
-        if open_file:
-            self._open_file()
-        else:
-            self._var = None
-            self._time = None
-            self._lat = None
-            self._lon = None
+        self._open_file = False
+        self._var = None
+        self._time = None
+        self._lat = None
+        self._lon = None
 
-    def _open_file(self):
+    def _init(self):
 
-        f = Nio.open_file(self.pathname)
+        if self._open_file is False:
+            self._open_file = True
+            f = Nio.open_file(self.pathname)
 
-        # Dimension of var is time, lat, lon
-        self._var = f.variables[self.varname]
-        self._time = f.variables['time']
-        self._lat = f.variables['lat']
-        self._lon = f.variables['lon']
-        if self.trange:
-            self.time = self._time[self.trange[0]:self.trange[1]]
-        else:
-            self.time = self._time[:]
-        self.lat = self._lat[:]
-        self.lon = self._lon[:]
+            # Dimension of var is time, lat, lon
+            self._var = f.variables[self.varname]
+            self._time = f.variables['time']
+            self._lat = f.variables['lat']
+            self._lon = f.variables['lon']
+            self.time = None
+            self.lat = None
+            self.lon = None
 
-    def get_var(self):
+    def get_var(self, chart=None):
 
-        if self._var is None:
-            self._open_file()
-        if self.trange:
-            return self._var[self.trange[0]:self.trange[1],:,:]
+        if self.trange is not None:
+            if self.trange[0] == self.trange[1]:
+                return None
+
+        if chart is not None:
+            if type(chart) is tuple:
+                if len(chart) != 2 or type(chart[0]) is not int or \
+                        type(chart[1]) is not int:
+                    raise TypeError, "chart must be a tuple of two integers"
+            elif type(chart) is not int:
+                raise TypeError, "chart must be an integer or tuple"
+
+            if self.trange is not None:
+                if type(chart) is int:
+                    if chart < 0 or chart >= self.trange[1]-self.trange[0]:
+                        raise IndexError, "chart is out of bound of trange"
+                if type(chart) is tuple:
+                    if chart[0] == chart[1]:
+                        return None
+                    if chart[0] > chart[1]:
+                        raise IndexError, "chart[1] must be larger than chart[0]"
+                    if chart[0] <0 or chart[0] >= self.trange[1]-self.trange[0]:
+                        raise IndexError, "chart[0] is out of bound of trange"
+                    if chart[1] <0 or chart[1] >= self.trange[1]-self.trange[0]:
+                        raise IndexError, "chart[1] is out of bound of trange"
+
+        self._init()
+
+        if type(chart) is int:
+            if self.trange is None:
+                return self._var[chart,:,:]
+            else:
+                return self._var[self.trange[0]+chart,:,:]
+        elif type(chart) is tuple:
+            if self.trange is None:
+                return self._var[chart[0]:chart[1],:,:]
+            else:
+                return self._var[self.trange[0]+chart[0]:self.trange[0]+chart[1],:,:]
         else:
             return self._var[:]
 
     def get_time(self):
-        if self._time is None:
-            self._open_file()
-        else:
-            return self.time
+
+        if self.trange is not None:
+            if self.trange[0] == self.trange[1]:
+                return None
+
+        self._init()
+        if self.time is None:
+            if self.trange is None:
+                self.time = self._time[:]
+            else:
+                self.time = self._time[self.trange[0]:self.trange[1]]
+        return self.time
 
     def get_lat(self):
-        if self._lat is None:
-            self._open_file()
+
+        self._init()
+        if self.lat is None:
+            self.lat = self._lat[:]
         return self.lat
 
     def get_lon(self):
-        if self._lon is None:
-            self._open_file()
+
+        self._init()
+        if self.lon is None:
+            self.lon = self._lon[:]
         return self.lon
+
+    def split(self, num):
+
+        if self._open_file is False:
+
+            if self.trange is not None:
+                time_len = self.trange[1]-self.trange[0]
+                tstart = self.trange[0]
+            else:
+                f = Nio.open_file(self.pathname)
+                time_len = f.dimensions['time']
+                f.close()
+                tstart = 0
+
+            chunk_size = time_len//num+1
+
+            tranges = [(tstart+min(i*chunk_size, time_len), tstart+ \
+                    min((i+1)*chunk_size, time_len)) for i in range(num)]
+
+            return [RectGrid(self.pathname, self.varname, trange=it) \
+                    for it in tranges]
+
+        else:
+            raise RuntimeError, "RectGrid must not be initialized before running split()"
 
     def _local_minima_func(self, buffer, size, threshold):
 
@@ -142,7 +211,6 @@ class RectGrid(Grid):
         if threshold == 0.:
             return search_window[origin] == search_window.min()
         elif search_window[origin] == search_window.min():
-            # # At least 1/2 of values in buffer should be larger than threshold
             # At least 8 of values in buffer should be larger than threshold
             return sorted(buffer)[8] - search_window[origin] > threshold
         else:
@@ -150,7 +218,9 @@ class RectGrid(Grid):
 
     def _local_minima_filter(self, input, size, threshold=0.):
 
-        assert size%2 == 1, "size must be an odd number"
+        if size%2 != 1:
+            raise ValueError, "size must be an odd number"
+
         half_size = size//2
 
         output = generic_filter(input, self._local_minima_func, size=size, \
@@ -174,6 +244,8 @@ class RectGrid(Grid):
 
     def detect(self, size=5, threshold=0.):
 
+        """Returns a list of list of Center's"""
+
         time = self.get_time()
         lat = self.get_lat()
         lon = self.get_lon()
@@ -182,38 +254,22 @@ class RectGrid(Grid):
 
         for it, t in enumerate(time):
 
-            chart = self.get_var()[it,:,:]
+            chart = self.get_var(chart=it)
             minima = self._local_minima_filter(chart, size, threshold=threshold)
             minima = self._remove_dup_laplace(chart, minima, size=5)
 
-            for i, j in np.transpose(minima.nonzero()):
-                c = Center(t, lat[i], lon[j], chart[i,j])
-                centers.append(c)
+            center_list = [Center(t, lat[i], lon[j], chart[i,j]) \
+                    for i, j in np.transpose(minima.nonzero())]
+            centers.append(center_list)
 
         return centers
 
-    def split(self, num):
-
-        if not self._time:
-
-            f = Nio.open_file(self.pathname)
-            time_len = f.dimensions['time']
-            f.close()
-
-            chunk_size = time_len//num+1
-
-            tranges = [(i*chunk_size, min((i+1)*chunk_size, time_len)) \
-                    for i in range(num)]
-
-            return [RectGrid(self.pathname, self.varname, trange=it, \
-                    open_file=False) for it in tranges]
-
-        else:
-            raise RunTimeError("open_file must be False to allow splitting")
-
 if __name__ == "__main__":
 
-    grid = RectGrid(pathname="../slp.2012.nc", varname="slp", trange=(0,1))
+    grid = RectGrid(pathname="../slp.2012.nc", varname="slp", trange=(0,10))
     centers = grid.detect()
 
     print(centers)
+
+    for c in centers:
+        print(c)
