bc321c61da033bd91ecf1d5486abe1929d5211e1
Albert Yau <mwyau@users.noreply.github.com>
2015-07-23 17:38:48 -0600

    Fix typo

--- scripts/run_ncep.sh
+++ scripts/run_ncep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ "$1" != ""]; then
-  mpirun -np 4 python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
+if [ "$1" != "" ]; then
+  mpirun -np $1 python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
 else
   python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
 fi
