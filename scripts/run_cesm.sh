345ec9cbeb0c113c2eccc562d6f5a147b40b38c0
Albert Yau <mwyau@users.noreply.github.com>
2015-07-23 16:53:32 -0600

    Add output filename to parameter list
    - Add -o or —output options to stormtracker.py
    - Fix typo in variable name in the CESM script

--- scripts/run_cesm.sh
+++ scripts/run_cesm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$1" != ""]; then
-  mpirun -np $1 python ../pystormtracker/stormtracker.py -i /Users/mwyau/Documents/CESM/b.e11.B20TRC5CNBDRD.f09_g16.031.cam.h2.PSL.1990010100Z-2005123118Z.nc -v SLP
+if [ "$1" != "" ]; then
+  mpirun -np $1 python ../pystormtracker/stormtracker.py -i /Users/mwyau/Documents/CESM/b.e11.B20TRC5CNBDRD.f09_g16.031.cam.h2.PSL.1990010100Z-2005123118Z.nc -v PSL -o tracks_cesm.pickle
 else
-  python ../pystormtracker/stormtracker.py -i /Users/mwyau/Documents/CESM/b.e11.B20TRC5CNBDRD.f09_g16.031.cam.h2.PSL.1990010100Z-2005123118Z.nc -v SLP
+  python ../pystormtracker/stormtracker.py -i /Users/mwyau/Documents/CESM/b.e11.B20TRC5CNBDRD.f09_g16.031.cam.h2.PSL.1990010100Z-2005123118Z.nc -v PSL -o tracks_cesm.pickle
 fi
--- scripts/run_ncep.sh
+++ scripts/run_ncep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$1" != ""]; then
-  mpirun -np 4 python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp
+  mpirun -np 4 python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
 else
-  python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp
+  python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
 fi
