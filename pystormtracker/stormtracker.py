f9f8a99d22d4dc2b9c1bc62a576ac162e62827f4
Albert Yau <mwyau@users.noreply.github.com>
2015-07-27 17:19:32 -0600

    Change MPI root to 0

--- pystormtracker/stormtracker.py
+++ pystormtracker/stormtracker.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
         size = comm.Get_size()
-        root = size-1
+        root = 0
 
     if USE_MPI is False or rank == root:
         timer['detector'] = timeit.default_timer()
