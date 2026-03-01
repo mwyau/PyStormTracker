d234ce4840c946d08cf38ba97fb71fc81dfade45
Albert Yau <5298134+mwyau@users.noreply.github.com>
2026-02-28 23:17:32 -0500

    Python 3 type hints and other refactor

--- src/pystormtracker/linker.py
+++ src/pystormtracker/linker.py
@@ -1,38 +1,41 @@
-from detector import RectGrid, Center
+from collections.abc import Iterator
+from typing import Any
 
-class Tracks(object):
+from .detector import Center, RectGrid
 
-    def __init__(self, threshold=500.):
 
-        self._tracks = []
-        self.head = []
-        self.tail = []
-        self.threshold = threshold
-        self.tstart = None
-        self.tend = None
-        self.dt = None
+class Tracks:
+    def __init__(self, threshold: float = 500.0) -> None:
 
-    def __getitem__(self, index):
+        self._tracks: list[list[Center]] = []
+        self.head: list[int] = []
+        self.tail: list[int] = []
+        self.threshold: float = threshold
+        self.tstart: Any | None = None
+        self.tend: Any | None = None
+        self.dt: Any | None = None
+
+    def __getitem__(self, index: int) -> list[Center]:
         return self._tracks[index]
 
-    def __setitem__(self, index, value):
+    def __setitem__(self, index: int, value: list[Center]) -> None:
         self._tracks[index] = value
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[list[Center]]:
         return iter(self._tracks)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._tracks)
 
-    def append(self, obj):
+    def append(self, obj: list[Center]) -> None:
         self._tracks.append(obj)
 
-    def match_center(self, centers):
+    def match_center(self, centers: list[Center]) -> list[int | None]:
 
         ends = [self[i][-1] for i in self.tail]
 
-        dforward = [{} for i in range(len(ends))]
-        dbackward = [{} for i in range(len(centers))]
+        dforward: list[dict[int, float]] = [{} for _ in range(len(ends))]
+        dbackward: list[dict[int, float]] = [{} for _ in range(len(centers))]
 
         for ic1, c1 in enumerate(ends):
             for ic2, c2 in enumerate(centers):
@@ -41,27 +44,24 @@ class Tracks(object):
                     dforward[ic1][ic2] = dist
                     dbackward[ic2][ic1] = dist
 
-        matched = [None for i in range(len(centers))]
+        matched: list[int | None] = [None for _ in range(len(centers))]
 
         while True:
-
             has_match = False
 
             for i, db in enumerate(dbackward):
-
-                if matched[i] is None and len(db)>0:
-
-                    iforward = min(db, key=db.get)
+                if matched[i] is None and len(db) > 0:
+                    iforward = min(db, key=db.get)  # type: ignore
                     di = dforward[iforward]
 
-                    if min(di, key=di.get) == i:
+                    if min(di, key=di.get) == i:  # type: ignore
                         matched[i] = iforward
 
-                        db = {}
+                        db.clear()
                         for j in dbackward:
                             if iforward in j:
                                 del j[iforward]
-                        di = {}
+                        di.clear()
 
                         for j in dforward:
                             if i in j:
@@ -70,33 +70,33 @@ class Tracks(object):
                         has_match = True
 
             if has_match is False:
-
                 break
 
         return [self.tail[i] if i is not None else None for i in matched]
 
-    def match_track(self, tracks):
+    def match_track(self, tracks: "Tracks") -> list[int | None]:
 
         centers = [tracks[i][0] for i in tracks.head]
         return self.match_center(centers)
 
-    def append_center(self, centers):
+    def append_center(self, centers: list[Center]) -> None:
 
-        new_tail = []
+        new_tail: list[int] = []
 
         matched_index = self.match_center(centers)
 
         for i, d in enumerate(matched_index):
-
             if self.tstart is None:
                 self.append([centers[i]])
-                self.head.append(len(self)-1)
-                new_tail.append(len(self)-1)
-            elif d is None or \
-                    self.tend is not None and self.dt is not None and \
-                    centers[0].time-self.dt > self.tend:
+                self.head.append(len(self) - 1)
+                new_tail.append(len(self) - 1)
+            elif d is None or (
+                self.tend is not None
+                and self.dt is not None
+                and centers[0].time - self.dt > self.tend
+            ):
                 self.append([centers[i]])
-                new_tail.append(len(self)-1)
+                new_tail.append(len(self) - 1)
             else:
                 self[d].append(centers[i])
                 new_tail.append(d)
@@ -109,43 +109,41 @@ class Tracks(object):
         elif self.dt is None:
             self.dt = self.tend - self.tstart
 
-    def extend_track(self, tracks):
+    def extend_track(self, tracks: "Tracks") -> None:
 
-        new_tail = []
+        new_tail: list[int] = []
 
         matched_index = self.match_track(tracks)
-        matched_dict = {d:matched_index[i] for i, d in enumerate(tracks.head)}
-        tail_dict = {d:None for d in tracks.tail}
+        matched_dict = {d: matched_index[i] for i, d in enumerate(tracks.head)}
+        tail_dict = dict.fromkeys(tracks.tail)
 
         for i, d in enumerate(tracks):
-            if i in matched_dict and matched_dict[i] is not None:
-                self[matched_dict[i]].extend(d)
+            match_idx = matched_dict.get(i)
+            if match_idx is not None:
+                self[match_idx].extend(d)
                 if i in tail_dict:
-                    new_tail.append(matched_dict[i])
+                    new_tail.append(match_idx)
             else:
                 self.append(d)
                 if i in tail_dict:
-                    new_tail.append(len(self)-1)
+                    new_tail.append(len(self) - 1)
 
         self.tail = new_tail
 
         self.tend = tracks.tend
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    import pickle
     import timeit
-    try:
-        import cPickle as pickle
-    except ImportError:
-        import pickle
 
     print("Starting detector...")
 
     timer = timeit.default_timer()
-    grid = RectGrid(pathname="../slp.2012.nc", varname="slp", trange=(0,120))
+    grid = RectGrid(pathname="../slp.2012.nc", varname="slp", trange=(0, 120))
     centers = grid.detect()
 
-    print("Detection time: " + str(timeit.default_timer()-timer))
+    print("Detection time: " + str(timeit.default_timer() - timer))
 
     print("Starting linker...")
 
@@ -156,43 +154,46 @@ if __name__ == "__main__":
     for c in centers:
         tracks.append_center(c)
 
-    print("Linking time: " + str(timeit.default_timer()-timer))
+    print("Linking time: " + str(timeit.default_timer() - timer))
 
-    num_tracks = len([t for t in tracks if len(t)>=8 and t[0].abs_dist(t[-1])>=1000.])
+    num_tracks = len(
+        [t for t in tracks if len(t) >= 8 and t[0].abs_dist(t[-1]) >= 1000.0]
+    )
 
-    print("Number of long tracks: "+str(num_tracks))
+    print("Number of long tracks: " + str(num_tracks))
 
-    pickle.dump(tracks, open("tracks.pickle", "wb"))
+    with open("tracks.pickle", "wb") as f:
+        pickle.dump(tracks, f)
 
     print("Starting multiple detector...")
 
     timer = timeit.default_timer()
 
-    grid2 = RectGrid(pathname="../slp.2012.nc", varname="slp", trange=(0,120))
+    grid2 = RectGrid(pathname="../slp.2012.nc", varname="slp", trange=(0, 120))
     grids2 = grid2.split(4)
 
     centers2 = [g.detect() for g in grids2]
 
-    print("Detection time: " + str(timeit.default_timer()-timer))
+    print("Detection time: " + str(timeit.default_timer() - timer))
 
     print("Starting multiple linker...")
 
     timer = timeit.default_timer()
 
-    tracks2 = [[] for i in range(4)]
+    tracks2: list[Tracks] = [Tracks() for _ in range(4)]
     for i, cs in enumerate(centers2):
-        tracks2[i] = Tracks()
-
         for c in cs:
             tracks2[i].append_center(c)
 
     tracksout = tracks2[0]
 
-    for i in range(1,4):
+    for i in range(1, 4):
         tracksout.extend_track(tracks2[i])
 
-    print("Detection time: " + str(timeit.default_timer()-timer))
+    print("Detection time: " + str(timeit.default_timer() - timer))
 
-    num_tracks = len([t for t in tracksout if len(t)>=8 and t[0].abs_dist(t[-1])>=1000.])
+    num_tracks = len(
+        [t for t in tracksout if len(t) >= 8 and t[0].abs_dist(t[-1]) >= 1000.0]
+    )
 
-    print("Number of long tracks: "+str(num_tracks))
+    print("Number of long tracks: " + str(num_tracks))
--- src/pystormtracker/stormtracker.py
+++ src/pystormtracker/stormtracker.py
@@ -1,34 +1,41 @@
-import sys, getopt
+import getopt
+import pickle
+import sys
 import timeit
-from glob import glob
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
+from typing import Literal
 
-from detector import RectGrid, Center
-from linker import Tracks
+from .detector import RectGrid
+from .linker import Tracks
 
 if __name__ == "__main__":
-
     try:
         from mpi4py import MPI
-        USE_MPI = True
+
+        USE_MPI: bool = True
     except ImportError:
         USE_MPI = False
 
-    trange=None
+    trange: tuple[int, int] | None = None
 
     argv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(argv,"hi:v:o:n:m:",["input=","var=","output=","num=","mode="])
+        opts, args = getopt.getopt(
+            argv, "hi:v:o:n:m:", ["input=", "var=", "output=", "num=", "mode="]
+        )
     except getopt.GetoptError:
         print("stormtracker.py -i <input file> -v <variable name> -o <output file>")
         sys.exit(2)
 
+    infile: str = ""
+    var: str = ""
+    outfile: str = ""
+    mode: Literal["min", "max"] = "min"
+
     for opt, arg in opts:
-        if opt == '-h':
-            print("stormtracker.py -i <input file> -v <variable name> -o <output file> -n <number of time steps>")
+        if opt == "-h":
+            print(
+                "stormtracker.py -i <input file> -v <variable name> -o <output file> -n <number of time steps>"
+            )
             sys.exit()
         elif opt in ("-i", "--input"):
             infile = arg
@@ -39,28 +46,31 @@ if __name__ == "__main__":
         elif opt in ("-n", "--num"):
             trange = (0, int(arg))
         elif opt in ("-m", "--mode"):
-            mode = arg
+            if arg in ("min", "max"):
+                mode = arg  # type: ignore
 
-    timer = {}
+    timer: dict[str, float] = {}
 
     if USE_MPI:
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
         size = comm.Get_size()
         root = 0
+    else:
+        rank = 0
+        root = 0
 
-    if USE_MPI is False or rank == root:
-        timer['detector'] = timeit.default_timer()
+    if not USE_MPI or rank == root:
+        timer["detector"] = timeit.default_timer()
 
     if USE_MPI:
-
         if rank == root:
-            grid = RectGrid(pathname=infile, varname=var, trange=trange)
-            grid = grid.split(size)
+            grid_obj = RectGrid(pathname=infile, varname=var, trange=trange)
+            grids = grid_obj.split(size)
         else:
-            grid = None
+            grids = None  # type: ignore
 
-        grid = comm.scatter(grid, root=root)
+        grid = comm.scatter(grids, root=root)
 
     else:
         grid = RectGrid(pathname=infile, varname=var, trange=trange)
@@ -69,9 +79,9 @@ if __name__ == "__main__":
 
     if USE_MPI:
         comm.Barrier()
-    if USE_MPI is False or rank == root:
-        timer['detector'] = timeit.default_timer()-timer['detector']
-        timer['linker'] = timeit.default_timer()
+    if not USE_MPI or rank == root:
+        timer["detector"] = timeit.default_timer() - timer["detector"]
+        timer["linker"] = timeit.default_timer()
 
     tracks = Tracks()
 
@@ -79,27 +89,30 @@ if __name__ == "__main__":
         tracks.append_center(c)
 
     if USE_MPI:
-
-        timer['combiner'] = timeit.default_timer()
+        timer["combiner"] = timeit.default_timer()
 
         nstripe = 2
         while nstripe <= size:
-            if rank%nstripe == nstripe/2:
-                comm.send(tracks, dest = rank-nstripe/2, tag=nstripe)
-            elif rank%nstripe == 0:
-                if rank+nstripe/2 < size:
-                    tracks_recv = comm.recv(source=rank+nstripe/2, tag=nstripe)
+            if rank % nstripe == nstripe // 2:
+                comm.send(tracks, dest=rank - nstripe // 2, tag=nstripe)
+            elif rank % nstripe == 0:
+                if rank + nstripe // 2 < size:
+                    tracks_recv = comm.recv(source=rank + nstripe // 2, tag=nstripe)
                     tracks.extend_track(tracks_recv)
-            nstripe = nstripe*2
+            nstripe = nstripe * 2
 
-        timer['combiner'] = timeit.default_timer()-timer['combiner']
+        timer["combiner"] = timeit.default_timer() - timer["combiner"]
 
-    if USE_MPI is False or rank == root:
-        timer['linker'] = timeit.default_timer()-timer['linker']
-        print("Detector time: "+str(timer['detector']))
-        print("Linker time: "+str(timer['linker']))
-        print("Combiner time: "+str(timer['combiner']))
-        num_tracks = len([t for t in tracks if len(t)>=8 and t[0].abs_dist(t[-1])>=1000.])
-        print("Number of long tracks: "+str(num_tracks))
+    if not USE_MPI or rank == root:
+        timer["linker"] = timeit.default_timer() - timer["linker"]
+        print("Detector time: " + str(timer["detector"]))
+        print("Linker time: " + str(timer["linker"]))
+        if "combiner" in timer:
+            print("Combiner time: " + str(timer["combiner"]))
+        num_tracks = len(
+            [t for t in tracks if len(t) >= 8 and t[0].abs_dist(t[-1]) >= 1000.0]
+        )
+        print("Number of long tracks: " + str(num_tracks))
 
-        pickle.dump(tracks, open(outfile, "wb"))
+        with open(outfile, "wb") as f:
+            pickle.dump(tracks, f)
