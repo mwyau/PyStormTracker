8ca0accfe10bdce6aa4f18e94104136193ceb932
Albert Yau <5298134+mwyau@users.noreply.github.com>
2026-03-04 17:57:04 -0500

    Update test data path

--- scripts/run_ncep.sh
+++ scripts/run_ncep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" != "" ]; then
-  mpirun -np $1 python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
+  mpirun -np $1 python ../pystormtracker/stormtracker.py -i ../data/test/slp.2012.nc -v slp -o tracks_ncep.pickle
 else
-  python ../pystormtracker/stormtracker.py -i ../slp.2012.nc -v slp -o tracks_ncep.pickle
+  python ../pystormtracker/stormtracker.py -i ../data/test/slp.2012.nc -v slp -o tracks_ncep.pickle
 fi
