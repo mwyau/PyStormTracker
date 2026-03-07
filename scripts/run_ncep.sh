cc86179ad6aaf5700ea9c23d0a8f5657f8901fa1
Albert Yau <5298134+mwyau@users.noreply.github.com>
2026-03-07 16:02:23 -0500

    chore: Remove deprecated scripts and notebooks

--- scripts/run_ncep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$1" != "" ]; then
-  mpirun -np $1 python ../pystormtracker/stormtracker.py -i ../data/test/slp.2012.nc -v slp -o tracks_ncep.pickle
-else
-  python ../pystormtracker/stormtracker.py -i ../data/test/slp.2012.nc -v slp -o tracks_ncep.pickle
-fi
