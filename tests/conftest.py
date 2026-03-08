9f20cebc5e37bc9c29793d30e6a40774b918b79a
Albert Yau <mwyau@users.noreply.github.com>
2026-03-08 01:45:30 -0500

    build: refine tiered testing logic with --run-slow and --run-all

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption(
-        "--run-slow", action="store_true", default=False, help="run slow tests"
+        "--run-slow", action="store_true", default=False, help="run ONLY slow tests"
     )
     parser.addoption(
         "--run-all", action="store_true", default=False, help="run all tests"
@@ -21,13 +21,10 @@ def pytest_collection_modifyitems(
     if run_all:
         return
 
-    skip_slow = pytest.mark.skip(reason="need --run-slow or --run-all option to run")
-    skip_fast = pytest.mark.skip(reason="skipped because --run-slow was given")
-
     for item in items:
-        if "slow" in item.keywords:
-            if not run_slow:
-                item.add_marker(skip_slow)
-        elif run_slow:
-            # If only --run-slow is given, skip the fast tests
-            item.add_marker(skip_fast)
+        is_slow = "slow" in item.keywords
+        if run_slow:
+            if not is_slow:
+                item.add_marker(pytest.mark.skip(reason="only slow tests requested"))
+        elif is_slow:
+            item.add_marker(pytest.mark.skip(reason="slow test skipped by default"))
