0b396818ca4d2893d8a9f913e440b6d746a34e66
Albert Yau <5298134+mwyau@users.noreply.github.com>
2026-03-10 01:07:54 -0400

    chore: bump version to v0.3.3-dev and add __version__

--- src/pystormtracker/__init__.py
+++ src/pystormtracker/__init__.py
@@ -1,4 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .models import Center, Grid, Tracks
 from .simple import SimpleDetector, SimpleLinker
+
+try:
+    __version__ = version("pystormtracker")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.3.3-dev"
 
 __all__ = ["Center", "Grid", "SimpleDetector", "SimpleLinker", "Tracks"]
