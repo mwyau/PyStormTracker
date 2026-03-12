0de87647e92bbe807dcb2808ae728226d47f700d
Albert Yau <5298134+mwyau@users.noreply.github.com>
2026-03-12 12:16:01 -0400

    test: implement --run-vo option, always show durations, and update legacy reference

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,9 @@ def pytest_addoption(parser: pytest.Parser) -> None:
         help="run ONLY integration tests",
     )
     parser.addoption(
+        "--run-vo", action="store_true", default=False, help="run VO integration tests"
+    )
+    parser.addoption(
         "--run-all", action="store_true", default=False, help="run all tests"
     )
 
